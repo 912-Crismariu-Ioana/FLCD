630a9593f8356b74ec98180d5be1e26d2bef0591
Ioana Teodora <52127822+912-Crismariu-Ioana@users.noreply.github.com>
2022-10-28 18:45:59 +0300

    commit after lab presentation

--- Lab2/src/components/Classifier.java
+++ Lab2/src/components/Classifier.java
@@ -3,7 +3,6 @@ package components;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
