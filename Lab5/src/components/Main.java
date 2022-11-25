549ca1d0247ad9deeb33ae31a24e5128e8fde906
Adrian-Ionut Craciun <adriancraciun128@gmail.com>
2022-11-25 18:50:14 +0200

    Lab5
    added g1, main and production utility class

---
+++ Lab5/src/components/Main.java
@@ -0,0 +1,23 @@
+import components.Grammar;
+import components.Production;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Grammar grammar = new Grammar("resources/g2.txt");
+        System.out.println(grammar.nonTerminalsToString());
+        System.out.println(grammar.terminalsToString());
+        System.out.println(grammar.productionsToString());
+        System.out.println("Productions for the non-terminal 'constant':");
+        System.out.println(grammar.printProductionsForNonTerminal("constant"));
+        if(grammar.isCFG()){
+            System.out.println("The grammar is context-free.");
+        }
+        else{
+            System.out.println("The grammar is not context-free.");
+        }
+    }
+}
